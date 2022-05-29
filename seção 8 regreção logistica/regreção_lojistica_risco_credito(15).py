17ba84b6b5c9f6715c465b2e32f94e4554bd3f08
Davi-ldc <96698943+Davi-ldc@users.noreply.github.com>
2022-05-29 10:55:05 -0300

    'master'

--- seção 8 regreção logistica/regreção_lojistica_risco_credito(15).py
+++ seção 8 regreção logistica/regreção_lojistica_risco_credito(15).py
@@ -1,1 +1,13 @@
-#a
+import pickle
+import numpy as np
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import accuracy_score
+
+with open('data/risco_credito.pkl', 'rb') as f:
+    dados_previsores, classes = pickle.load(f)
+    
+dados_previsores = np.delete(dados_previsores, 2, 7, 11, axis=0)
+#                                                indices  1 apaga colunas   0 apaga linhas
+classes = np.delete(classes, 2, 7, 11, axis=0)
+
+regreção_lojistica = LogisticRegression()
