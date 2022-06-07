b994476c6d23eac7127850588dd43963159da0ce
Davi-ldc <96698943+Davi-ldc@users.noreply.github.com>
2022-06-07 07:29:43 -0300

    👀

--- seção 11 avaliação de algoritimos d classificação/ajuste_dos_parametros_de_algoritimos.py
+++
@@ -1,8 +0,0 @@
-import pickle
-from sklearn.model_selection import GridSearchCV
-from sklearn.neural_network import MLPClassifier
-
-with open('data/census.pkl', 'rb') as f:
-    dados_previsores_treinamento, dados_classe_treinamento, dados_previsores_test, classes_test = pickle.load(f)
-
-r_d = MLPClassifier()
