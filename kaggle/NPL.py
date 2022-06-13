6b9fd48f6b73bec8a2f6ec31fca1bf2191d9dbd3
Davi-ldc <96698943+Davi-ldc@users.noreply.github.com>
2022-06-13 10:08:36 -0300

    Update NPL.py

--- kaggle/NPL.py
+++ kaggle/NPL.py
@@ -4,20 +4,13 @@ import numpy as np
 from sklearn.metrics import accuracy_score
 from sklearn.feature_extraction.text import CountVectorizer
 
-#carrega os dados
-with open('data/emotions_train.txt') as f:
-    treinamento = pd.read_csv(f, sep = ';', names=["emotion", "sentence"])
+treinamento = pd.read_csv("https://raw.githubusercontent.com/Davi-ldc/machine-learnig/master/data/emotions_train.txt", sep=";", names=["sentence", "emotion"])
+teste = pd.read_csv("https://raw.githubusercontent.com/Davi-ldc/machine-learnig/master/data/emotions_test.txt", sep=";", names=["sentence", "emotion"])
+data = treinamento.append(teste)
 
-with open('data/emotions_test.txt') as f:
-    teste = pd.read_csv(f, sep = ';', names=["emotion", "sentence"])
-    
-dados = treinamento.append(teste)
 
 cv = CountVectorizer()
-vetor = cv.fit(dados['sentence'])
-
-
-#divide os dados
+vector = cv.fit(data['sentence'])
 
 dados_previsores_treinamento = treinamento.iloc[:, 0].values
 classes_treinamento = treinamento.iloc[:, 1].values
@@ -25,8 +18,8 @@ classes_treinamento = treinamento.iloc[:, 1].values
 dados_previsores_teste = teste.iloc[:, 0].values
 classes_teste = teste.iloc[:, 1].values
 
-dados_previsores_treinamento = vetor.transform(dados_previsores_treinamento)
-dados_previsores_teste = vetor.transform(dados_previsores_teste)
+dados_previsores_treinamento = vector.transform(dados_previsores_treinamento)
+dados_previsores_teste = vector.transform(dados_previsores_teste)
 
 def emotions(string,vector,model):
      vectorized = vector.transform([string])
@@ -36,7 +29,7 @@ def emotions(string,vector,model):
 #aplica a rede neural
 from sklearn.neural_network import MLPClassifier
 
-rede_neural = MLPClassifier(verbose=True, max_iter=1000, tol=0.00001, random_state=0)
+rede_neural = MLPClassifier(verbose=True, max_iter=1000, tol=0.0000001, hidden_layer_sizes=(50, 50, 50))
 
 #treina a rede neural
 rede_neural.fit(dados_previsores_treinamento, classes_treinamento)
@@ -48,10 +41,3 @@ previsoes = rede_neural.predict(dados_previsores_teste)
 #pontuação
 pontuação = accuracy_score(classes_teste, previsoes)
 print(pontuação)
-
-from yellowbrick.classifier import ClassificationReport
-
-cm = ClassificationReport(rede_neural)
-cm.fit(dados_previsores_treinamento, classes_treinamento)
-cm.score(dados_previsores_teste, classes_teste)
-cm.poof()
