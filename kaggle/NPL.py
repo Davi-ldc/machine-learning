8780f7198cc4d36fb5291fe821def21eec77d6c7
Davi-ldc <96698943+Davi-ldc@users.noreply.github.com>
2022-06-13 15:14:41 -0300

    👀👀

--- kaggle/NPL.py
+++ kaggle/NPL.py
@@ -3,7 +3,6 @@ import numpy as np
 from sklearn.metrics import accuracy_score
 from sklearn.feature_extraction.text import CountVectorizer
 
-<<<<<<< HEAD
 
 treinamento = pd.read_csv("https://raw.githubusercontent.com/Davi-ldc/machine-learnig/master/data/emotions_train.txt", sep=";", names=["sentence", "emotion"])
 teste = pd.read_csv("https://raw.githubusercontent.com/Davi-ldc/machine-learnig/master/data/emotions_test.txt", sep=";", names=["sentence", "emotion"])
@@ -45,14 +44,6 @@ plt.ylabel("Frequence")
 o que tem menos dados é o que ele mais errou"""
 
 
-=======
-treinamento = pd.read_csv("https://raw.githubusercontent.com/Davi-ldc/machine-learnig/master/data/emotions_train.txt", sep=";", names=["sentence", "emotion"])
-teste = pd.read_csv("https://raw.githubusercontent.com/Davi-ldc/machine-learnig/master/data/emotions_test.txt", sep=";", names=["sentence", "emotion"])
-data = treinamento.append(teste)
-
-treinamento.head(27)
-
->>>>>>> 8c68ff1f07f05fb06280d92f3b6b4aa00d490497
 
 cv = CountVectorizer()
 vector = cv.fit(data['sentence'])
@@ -74,11 +65,7 @@ def emotions(string,vector,model):
 #aplica a rede neural
 from sklearn.neural_network import MLPClassifier
 
-<<<<<<< HEAD
 rede_neural = MLPClassifier(verbose=True, max_iter=1000, tol=0.0000001, hidden_layer_sizes=(50, 50, 50), random_state=0)
-=======
-rede_neural = MLPClassifier(verbose=True, max_iter=1000, tol=0.0000001, hidden_layer_sizes=(50, 50, 50))
->>>>>>> 8c68ff1f07f05fb06280d92f3b6b4aa00d490497
 
 #treina a rede neural
 rede_neural.fit(dados_previsores_treinamento, classes_treinamento)
@@ -88,13 +75,8 @@ rede_neural.fit(dados_previsores_treinamento, classes_treinamento)
 previsoes = rede_neural.predict(dados_previsores_teste)
 
 #pontuação
-<<<<<<< HEAD
 pontuação = accuracy_score(classes_teste, previsoes)
 print(pontuação)#0.8525
-=======
-pontuação = accuracy_score(classes_teste, previsoes)#0.8435
-print(pontuação)
->>>>>>> 8c68ff1f07f05fb06280d92f3b6b4aa00d490497
 
 from yellowbrick.classifier import ConfusionMatrix, ClassificationReport
 cm = ClassificationReport(rede_neural)
@@ -113,7 +95,3 @@ while True:
   txt = input("escreve algo")
   print(txt)
   print(emotions(txt, vector, rede_neural))
-<<<<<<< HEAD
-=======
-
->>>>>>> 8c68ff1f07f05fb06280d92f3b6b4aa00d490497
