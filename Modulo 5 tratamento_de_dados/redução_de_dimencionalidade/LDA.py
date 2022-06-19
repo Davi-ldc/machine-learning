52bbeb30ed51904d7ee65d5ff7bd1e404ba357e2
Davi-ldc <96698943+Davi-ldc@users.noreply.github.com>
2022-06-19 11:09:26 -0300

    👀👀'

---
+++ Modulo 5 tratamento_de_dados/redução_de_dimencionalidade/LDA.py
@@ -0,0 +1,3 @@
+#Linear Discriminant Analysis(LDA) (supervisionado)
+
+
---
+++ Modulo 5 tratamento_de_dados/redução_de_dimencionalidade/PCA.py
@@ -0,0 +1,9 @@
+#Principal component analysis (PCA) (não supervisionado)
+#é um método de dimensionamento de dados que utiliza
+#a projeção de componentes principais para reduzir a dimensão de um conjunto de dados.
+
+#identifica a corelação entre as variaveis que, caso seja forte, é possivel reduzir
+#a dimencionalidade
+
+#quando o problema é mais complexo ou os dados não saõ linearmente separaveis
+#vc pd usar o kernel PCA 
