4f761d266f672c75e40a68a7316917cc13413abb
Davi-ldc <96698943+Davi-ldc@users.noreply.github.com>
2022-06-24 18:03:37 -0300

    👀👀

--- teste_keras.py
+++ teste_keras.py
@@ -4,22 +4,22 @@ from sklearn.preprocessing import LabelEncoder
 from sklearn.metrics import accuracy_score
 from sklearn.model_selection import train_test_split
 
-# with open('data/census.csv') as f:
-#     data = pd.read_csv(f)
+with open('data/census.csv') as f:
+    data = pd.read_csv(f)
     
-# variaveis_previsoras = data.iloc[:, 0:14].values
-# classes = data.iloc[:, 14].values
+variaveis_previsoras = data.iloc[:, 0:14].values
+classes = data.iloc[:, 14].values
 
-# label = LabelEncoder() 
-# for c in range(1, 14): 
-#     if c == 2 or c == 10 or c == 11 or c == 12: # colunas que ja sao numericas 
-#         continue 
-#     variaveis_previsoras[:, c] = label.fit_transform(variaveis_previsoras[:, c]) 
+label = LabelEncoder() 
+for c in range(1, 14): 
+    if c == 2 or c == 10 or c == 11 or c == 12: # colunas que ja sao numericas 
+        continue 
+    variaveis_previsoras[:, c] = label.fit_transform(variaveis_previsoras[:, c]) 
     
-#     Q_numero_cada_str_recebeu = dict(zip(label.classes_, label.transform(label.classes_)))
-#     # print(Q_numero_cada_str_recebeu)
+    Q_numero_cada_str_recebeu = dict(zip(label.classes_, label.transform(label.classes_)))
+    # print(Q_numero_cada_str_recebeu)
 
-# variaveis_previsoras_treino, variaveis_previsoras_teste, classes_treino, classes_teste = train_test_split(variaveis_previsoras, classes, test_size=0.3, random_state=0)
+variaveis_previsoras_treino, variaveis_previsoras_teste, classes_treino, classes_teste = train_test_split(variaveis_previsoras, classes, test_size=0.3, random_state=0)
 
  
  
