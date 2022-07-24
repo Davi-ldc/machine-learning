842602d2d26a1f39396645c50ed1ddc9f7eff57b
Davi-ldc <96698943+Davi-ldc@users.noreply.github.com>
2022-07-24 12:52:16 -0300

    👀👀

---
+++ DL/boltzmann machines/teoria.py
@@ -0,0 +1,5 @@
+#tds os neuronios estão interligados de forma circular
+#n possue camada d saida
+#serve para recomendação e redução d dimencionalidade
+#não supervisionado
+#aprende o comportamento padrão de um sistema, o q pd ser usado para detectar anomalias
