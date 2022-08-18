86638415b77ae9ff5f9b14c429fb7a6934e4197a
Davi-ldc <96698943+Davi-ldc@users.noreply.github.com>
2022-08-18 15:42:25 -0300

    👀👀

--- GANs/MUSIC/MUSICA.py
+++ GANs/MUSIC/MUSICA.py
@@ -8,3 +8,17 @@ from keras.models import Sequential
 from keras.optimizers_v2.adam_v2 import Adam
 from sklearn.preprocessing import MinMaxScaler, StandardScaler
 
+#mid é tipo mp3 só q é mlhr pro python
+notas = []
+for song in os.listdir('/content/drive/MyDrive/data_musica'):
+    mid = MidiFile('/content/drive/MyDrive/data_musica/' + song)
+    print(mid)
+    for msg in mid:
+        if not msg.is_meta and msg.channel == 0 and msg.type =='note_on':
+            data = msg.bytes()
+            notas.append(data[1])
+print(notas)
+
+#deixa as notas na msm escala 
+scaler = MinMaxScaler()
+notes = list(scaler.fit_transform(np.array(notas).reshape(-1,1)))
