61bc0d142f8924f666bc56de116c1eaf34300429
Davi-ldc <96698943+Davi-ldc@users.noreply.github.com>
2022-08-27 11:56:18 -0300

    👀👀

---
+++ Cycle_Gans/toeria.py
@@ -0,0 +1,13 @@
+"""
+sereve para "traduzir" imagens  tipo imagina q vc qr pegar um cavalo e transformar ele
+numa zebra. vc tenq garantir que: vai se parcer como uma zebra, vai se parecer com
+a imgem horiginal ai pra frz isso vc cria um gerador pra 
+
+
+
+
+"""
+from re import M
+from keras.layers import BatchNormalization
+
+
---
+++ GANs/Cachorros/gerando_cachoros.py
@@ -0,0 +1,20 @@
+import numpy as np
+import tensorflow as tf
+import matplotlib.pyplot as plt
+from keras.models import load_model
+from keras.utils import array_to_img
+
+num_img = 10
+tamanho_do_ruido = 128
+ruido = tf.random.normal(shape=(num_img, tamanho_do_ruido))
+print("Carregando modelo...")
+Gerador = load_model('modelos\Gerador_dogs.h5')
+print("Modelo carregado!")
+imgs_geradas = Gerador(ruido)
+imgs_geradas *= 255
+imgs_geradas.numpy()
+
+for i in range(num_img):
+    img = array_to_img(imgs_geradas[i])
+    plt.imshow(img)
+    plt.show()
